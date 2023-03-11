2f9d6126267482164cb9c314c37e57a7e8de1fa6
kerezsiz42 <kerezsiz42@gmail.com>
2023-03-11 10:08:11 +0100

    project setup

---
+++ backend/src/main/java/com/example/pextus/PextusApplication.java
@@ -0,0 +1,13 @@
+package com.example.pextus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PextusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PextusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/pextus/PextusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pextus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PextusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
