df8e717b15ee33e2ca190d004d701f8d5cd94fa2
kerezsiz42 <kerezsiz42@gmail.com>
2023-03-12 18:41:04 +0100

    - add logging in book and writer controller - configure logback logger

--- backend/src/main/java/com/example/pextus/BookController.java
+++ backend/src/main/java/com/example/pextus/BookController.java
@@ -1,5 +1,7 @@
 package com.example.pextus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,8 +9,9 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "api/v1/books")
+@RequestMapping(path = "/api/v1/books")
 public class BookController {
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
     private final BookService bookService;
     @Autowired
     public BookController(BookService bookService) {
@@ -17,6 +20,7 @@ public class BookController {
 
     @GetMapping(produces = "application/json")
     public ResponseEntity<List<Book>> getBooks(@RequestParam(name = "q", defaultValue = "") String title) {
+        logger.info("HTTP GET /api/v1/books");
         if (title.equals("")) {
             return ResponseEntity.ok(this.bookService.getAllBooks());
         }
@@ -25,16 +29,19 @@ public class BookController {
 
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<Optional<Book>> getBook(@PathVariable String id) {
+        logger.info("HTTP GET /api/v1/books/{id}");
         return ResponseEntity.ok(this.bookService.findBookById(id));
     }
 
     @PutMapping(produces = "application/json")
     public ResponseEntity<Book> putBook(@RequestBody Book book) {
+        logger.info("HTTP PUT /api/v1/books");
         return ResponseEntity.ok(this.bookService.createOrModifyBook(book));
     }
 
     @DeleteMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<String> deleteBook(@PathVariable String id) {
+        logger.info("HTTP DELETE /api/v1/books/{id}");
         this.bookService.deleteBookById(id);
         return ResponseEntity.ok(id);
     }
--- backend/src/main/java/com/example/pextus/WriterController.java
+++ backend/src/main/java/com/example/pextus/WriterController.java
@@ -1,5 +1,7 @@
 package com.example.pextus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping(path = "api/v1/writers")
 public class WriterController {
+    private static final Logger logger = LoggerFactory.getLogger(WriterController.class);
     private final WriterService writerService;
     @Autowired
     public WriterController(WriterService writerService) {
@@ -18,22 +21,26 @@ public class WriterController {
 
     @GetMapping(produces = "application/json")
     public ResponseEntity<List<Writer>> getWriters() {
+        logger.info("HTTP GET /api/v1/writers");
         return ResponseEntity.ok(this.writerService.getWriters());
     }
 
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<Optional<Writer>> getWriter(@PathVariable String id) {
+        logger.info("HTTP GET /api/v1/writers/{id}");
         Optional<Writer> writer = this.writerService.findWriterById(id);
         return ResponseEntity.ok(writer);
     }
 
     @PutMapping(produces = "application/json")
     public ResponseEntity<Writer> putWriter(@RequestBody Writer writer) {
+        logger.info("HTTP PUT /api/v1/writers");
         return ResponseEntity.ok(this.writerService.createOrModifyWriter(writer));
     }
 
     @DeleteMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<String> deleteWriter(@PathVariable String id) {
+        logger.info("HTTP DELETE /api/v1/writers/{id}");
         this.writerService.deleteBookById(id);
         return ResponseEntity.ok(id);
     }
