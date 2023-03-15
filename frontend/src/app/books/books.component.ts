2b0bd8bf7460e0f833563c00a6775a3e55400f7e
kerezsiz42 <kerezsiz42@gmail.com>
2023-03-15 14:00:07 +0100

    - enable cors on backend - test book fetching

--- frontend/src/app/books/books.component.ts
+++ frontend/src/app/books/books.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'books',
-  templateUrl: './books.component.html'
+  templateUrl: './books.component.html',
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('http://localhost:8080/api/v1/books')
+      .subscribe((res) => console.log(res));
+  }
 }
